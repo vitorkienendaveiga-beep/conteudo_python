c6b8e95eb288bd73c3b76c37b6d534e1b9d1b044
Aluno SENAI <Aluno@SN-388037.edu.fiesc.com.br>
2025-10-03 09:50:02 -0300

    aula1: comando print

---
+++ aula1.py
@@ -0,0 +1,14 @@
+#Isso é um comentario
+#vamos escapar da maldição
+
+#Esse é o comando print,
+#ele serve para imprimir na tela
+print("Olá mundo!")
+
+#Podemos mudar o comportamento da finalização
+#do comando com o parâmetro end
+print("Esse é o curso de:",end=" ")
+print("Lógica","de","programação")
+#Podemos mudar o comportamento do separador
+#com o parâmetro sep
+print("frase","com","hifen",sep="-")
